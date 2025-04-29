67cd5ef1adf2e15d06be117da9692a602e4c2cc0
sabrinacinque <cinque.sabrina@gmail.com>
2025-04-29 22:08:20 +0200

    starter generation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmListComponent } from './components/film-list/film-list.component';
+import { NavbarComponent } from './components/mainComponents/navbar/navbar.component';
+import { FooterComponent } from './components/mainComponents/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmListComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/film-list/film-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-film-list',
+  templateUrl: './film-list.component.html',
+  styleUrl: './film-list.component.css'
+})
+export class FilmListComponent {
+
+}
