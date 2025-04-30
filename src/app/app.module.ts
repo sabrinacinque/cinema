ea57e9a6bc6105c222db4bb95ec06796fcd97614
sabrinacinque <cinque.sabrina@gmail.com>
2025-04-30 23:04:44 +0200

    aggiustato courosel ,eliminato material e installato bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { FilmListComponent } from './components/film-list/film-list.component';
 import { NavbarComponent } from './components/mainComponents/navbar/navbar.component';
 import { FooterComponent } from './components/mainComponents/footer/footer.component';
 import { DettagliFilmComponent } from './components/dettagli-film/dettagli-film.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
@@ -27,14 +21,8 @@ import { MatListModule } from '@angular/material/list';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    MatCardModule,
-    MatGridListModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule
+    HttpClientModule
+
   ],
   providers: [
     provideClientHydration(),
--- src/app/components/film-list/film-list.component.ts
+++ src/app/components/film-list/film-list.component.ts
@@ -1,5 +1,5 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FilmService } from './../../services/film.service';
-import { Component, OnInit } from '@angular/core';
 import { IFilm } from '../../models/ifilm';
 
 @Component({
@@ -8,15 +8,47 @@ import { IFilm } from '../../models/ifilm';
   styleUrls: ['./film-list.component.css']
 })
 export class FilmListComponent implements OnInit {
-
   filmList: IFilm[] = [];
+  visibleFilms: IFilm[] = [];
+
+  @ViewChild('carousel', { static: false }) carousel!: ElementRef;
 
   constructor(private filmService: FilmService) {}
 
   ngOnInit(): void {
     this.filmService.getFilms().subscribe({
-      next: (data) => this.filmList = data,
+      next: (data) => {
+        this.filmList = data;
+        this.visibleFilms = [...this.filmList, ...this.filmList]; // Loop visivo
+      },
       error: (err) => console.error('Errore nel recupero film', err)
     });
+  }
+
+  scrollRight() {
+    const carousel = this.carousel.nativeElement;
+    const maxScroll = carousel.scrollWidth - carousel.clientWidth;
+
+    if (carousel.scrollLeft + 220 >= maxScroll) {
+      // Scroll reached end, restart from 0
+      carousel.scrollTo({ left: 0, behavior: 'smooth' });
+    } else {
+      carousel.scrollBy({ left: 220, behavior: 'smooth' });
+    }
+  }
+
+  scrollLeft() {
+    const carousel = this.carousel.nativeElement;
+
+    if (carousel.scrollLeft <= 0) {
+      // Scroll at start, jump to end
+      carousel.scrollTo({ left: carousel.scrollWidth, behavior: 'smooth' });
+    } else {
+      carousel.scrollBy({ left: -220, behavior: 'smooth' });
+    }
+  }
+
+  get loopArray() {
+    return Array(10).fill(0);
   }
 }
